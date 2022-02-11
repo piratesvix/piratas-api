dacf3634fbe0b50b483173c458f2665e7f775f47
ahsouza <annibalhsouza@gmail.com>
2022-02-11 18:38:59 -0300

    feat: implementar renderização das páginas html

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
-
+# Rota Inicial
 @app.route("/")
 def hello_world():
-    return "<p>Hello, PiratasWEB!</p>"
+    return render_template('index.html')
