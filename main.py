17811979c67b9223f103dc274b0b0012e8e48ed4
ahsouza <annibalhsouza@gmail.com>
2022-02-13 12:30:33 -0300

    feat: implementar conexão de instância de banco de dados MongoDB e adicionar rota para inserir dados

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, request, jsonify
+from flask_pymongo import PyMongo
 
+# instanciando recurso flask para criar servidor e armazenar em variável 'app'
 app = Flask(__name__, template_folder='templates')
 
-# Rota Inicial
+# uri - faça a conexão com banco de dados MongoDB utilizando variáveis de ambiente do sistema operacional
+app.config["MONGO_URI"] = 'mongodb://' + os.environ['MONGODB_USERNAME'] + ':' + os.environ['MONGODB_PASSWORD'] + '@' + os.environ['MONGODB_HOSTNAME'] + ':27017/' + os.environ['MONGODB_DATABASE']
+
+# atribuindo recursos mongo python em variável
+mongo = PyMongo(app)
+db = mongo.db
+
+# Rota Inicial renderizando template
 @app.route("/")
 def hello_world():
     return render_template('index.html')
+
+# Rota para persistir dados em MongoDB 
+@app.route("/new/piratas")
+def add_pirates():
+    db.piratas.insert_many([
+        {'_id': 1, 'nome': "Henrique", 'sobrenome':"Souza", 'email':"annibalhsouza@gmail.com", 'idade': "29", 'cargo': "fundador/CTO",  'genero': "masculino", 'github': "ahsouza", 'url': "https://ahsouza.github.io/", 'linkedin': "anibalhenriquesouza", 'skills': "rust - go - js - ts - html - css - java - c/c++ - c# - py - shell - linux/windows/darwin" },
+        {'_id': 2, 'nome': "William", 'sobrenome':"Agostini", 'email':"willsantos96@gmail.com", 'idade': "27", 'cargo': "Sócio Desenvolvedor", 'genero': "masculino", 'github': "willsantos96", 'url': "https://williamagostini.github.io/", 'linkedin': "william-agostini", 'skills': "js - html - css - py - shell - docker - linux/windows"},
+        ])
+    return jsonify(message="Pirata adicionado", status="success"), 201
