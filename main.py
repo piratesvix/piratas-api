880f4039fc5f661400e3901dec66cbbc12022c18
ahsouza <annibalhsouza@gmail.com>
2022-03-07 17:14:54 -0300

    feat: adicionar estrutura de código para executar a aplicação usando variáveis de ambiente, parâmetros e debugs.

--- main.py
+++ main.py
@@ -117,3 +117,8 @@ def novo_projeto():
     except:
         # Erro ao tentar criar o recurso
         return "", 500
+# executar api
+if __name__ == "__main__":
+    ENVIRONMENT_DEBUG = os.environ.get("APP_DEBUG", True)
+    ENVIRONMENT_PORT = os.environ.get("APP_PORT", 5000)
+    app.run(host='0.0.0.0', port=ENVIRONMENT_PORT, debug=ENVIRONMENT_DEBUG)
